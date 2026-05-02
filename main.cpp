a56baffc02d71bf65f9a2c8dec26cdd2c49e5230
Alberto Torres <acc.dummyagta1@gmail.com>
2026-05-01 23:15:17 -0500

    Added runtime figure creation thorugh terminal commands

--- main.cpp
+++ main.cpp
@@ -2,7 +2,30 @@
 #include "figures/Figure.hpp"
 #include "figures/Line.hpp"
 #include "figures/Rectangle.hpp"
+
+#include <optional>
 #include <print>
+#include <thread>
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+
+std::optional<figures::Figure> parse_figure(std::istream& iss) {
+  std::string cmd;
+  iss >> cmd;
+
+  if (cmd == "rect") {
+    int x, y, w, h;
+    if (iss >> x >> y >> w >> h)
+      return figures::Rectangle(x, y, w, h, Colors::RED);
+  } else if (cmd == "line") {
+    int x1, y1, x2, y2;
+    if (iss >> x1 >> y1 >> x2 >> y2)
+      return figures::Line(x1, y1, x2, y2, Colors::WHITE);
+  }
+  return std::nullopt;
+}
 
 int main() {
   auto window = Window::create();
@@ -16,11 +39,25 @@ int main() {
     figures::Line(10, 10, 200, 150, Colors::WHITE),
   };
 
-  window->run([&figures](Renderer& r, GLFWwindow*) {
+  std::mutex figures_mutex;
+
+  std::thread([&]() {
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+      std::istringstream iss(line);
+      if (auto figure = parse_figure(iss)) {
+        std::scoped_lock lock(figures_mutex);
+        figures.emplace_back(std::move(*figure));
+      }
+    }
+  }).detach();
+
+  window->run([&](Renderer& r, GLFWwindow*) {
     r.clear(Colors::BLACK);
+
+    std::scoped_lock lock(figures_mutex);
     for (auto&& figure : figures)
       figure.draw(r);
   });
 }
-
-//testing
