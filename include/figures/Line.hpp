ac4a1e7ce189de244b710dbabbd8bc64517c13c4
Alberto Torres <acc.dummyagta1@gmail.com>
2026-05-05 03:41:55 -0500

    Changed Bresenham's algorithm back to original idea

--- include/figures/Line.hpp
+++ include/figures/Line.hpp
@@ -8,8 +8,6 @@
 #include <cstddef>
 
 namespace figures {
-// r.width() >= x1 >= x0
-// r.height() >= y1 >= y0
 struct Line {
   std::size_t x0;
   std::size_t y0;
@@ -19,19 +17,35 @@ struct Line {
 
   //Bresenham's line algorithim
   void draw(Renderer& r) const {
-    std::ptrdiff_t dx = x1 - x0;
-    std::ptrdiff_t dy = y1 - y0;
+    std::ptrdiff_t x0 = static_cast<std::ptrdiff_t>(this->x0);
+    std::ptrdiff_t y0 = static_cast<std::ptrdiff_t>(this->y0);
+    std::ptrdiff_t x1 = static_cast<std::ptrdiff_t>(this->x1);
+    std::ptrdiff_t y1 = static_cast<std::ptrdiff_t>(this->y1);
 
-    std::ptrdiff_t delta = 2 * dy - dx;
+    std::ptrdiff_t dx = std::abs(x1 - x0);
+    std::ptrdiff_t dy = std::abs(y1 - y0);
 
-    std::size_t y = y0;
-    for (std::size_t x = x0; x <= x1; x++) {
-      r[x, y] = color;
-      if (delta > 0) {
-        y++;
-        delta += 2 * (dy - dx);
-      } else {
-        delta += 2 * dy;
+    std::ptrdiff_t sx = (x0 < x1) ? 1 : -1;
+    std::ptrdiff_t sy = (y0 < y1) ? 1 : -1;
+
+    std::ptrdiff_t err = dx - dy;
+
+    while (true) {
+      r[x0, y0] = color;
+
+      if (x0 == x1 && y0 == y1)
+        break;
+
+      std::ptrdiff_t e2 = 2 * err;
+
+      if (e2 > -dy) {
+        err -= dy;
+        x0 += sx;
+      }
+
+      if (e2 < dx) {
+        err += dx;
+        y0 += sy;
       }
     }
   }
